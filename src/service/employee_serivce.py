505744f849c2406c682c1c7d64e6c76cf6e9fb51
pavan.kasula@thrymr.net-at-175029991794 <pavan.kasula@thrymr.net>
2024-01-05 16:44:22 +0530

    first

---
+++ src/service/employee_serivce.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+from src.dto.beans import  EmployeeDetails
+
+
+class EmployeeService(ABC):
+    @abstractmethod
+    def save_employee(self,employee_details:EmployeeDetails):
+        pass
+
